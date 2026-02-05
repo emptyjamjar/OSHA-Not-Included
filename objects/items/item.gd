732815a144a65238744f6761523e9f94b1365eef
Devanush Joseph <sek402@usask.ca>
2026-02-04 23:56:27 -0600

    Initial Package type complete

--- objects/items/item.gd
+++ objects/items/item.gd
@@ -1,5 +1,8 @@
-class_name Item extends Node
+class_name Item extends Node2D
 
 # Set these attributes in ready
 var shipmentTime : int
 var stealable : bool
+# How much space it takes up in inventories
+var size : int
+var weight : float
---
+++ objects/package/package.gd
@@ -0,0 +1,31 @@
+class_name Package extends Node2D
+
+var inventory : Array[Item]
+var capacity : int
+# Used to differentiate between modifying contents and holding
+var isHoldable : bool
+
+@onready var iArea := $InteractionArea
+@onready var hitboxShape := $Hitbox/CollisionShape2D
+
+func _ready() -> void:
+	iArea.interact = Callable(self, "_on_interact")
+	isHoldable = true
+
+func disable():
+	# Disable Collision
+	hitboxShape.disabled = not hitboxShape.disabled
+	# Disable Interaction
+	iArea.monitoring = not iArea.monitoring
+	iArea.monitorable = not iArea.monitoring
+
+func _on_interact():
+	# if the station changes holdable then pick it up
+	if isHoldable:
+		# temporary code until inventory is set up
+		var player = get_tree().get_first_node_in_group("player")
+		if player:
+			disable()
+			global_position = player.global_position
+			reparent(player, true)
+		
