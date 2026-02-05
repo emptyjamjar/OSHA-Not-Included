94c78ad74c5eaf44977ba3dc77e384631e4e17d2
Devanush Joseph <sek402@usask.ca>
2026-02-04 20:07:17 -0600

    Finished InteractionArea script

--- core/components/interaction-area/interaction_area.gd
+++ core/components/interaction-area/interaction_area.gd
@@ -5,3 +5,11 @@ class_name InteractionArea extends Area2D
 # Will be overriden by scenes that instantiate InteractionArea
 var interact: Callable = func():
 	pass
+
+
+func _on_body_entered(_body: Node2D) -> void:
+	InteractionManager.register_area(self)
+
+
+func _on_body_exited(_body: Node2D) -> void:
+	InteractionManager.unregister_area(self)
