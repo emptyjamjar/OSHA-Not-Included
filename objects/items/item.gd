7ec3fad7d77a1217882a828468104155e7eec718
Devanush Joseph <sek402@usask.ca>
2026-02-09 12:34:21 -0600

    Added default values for init parameters

--- objects/items/item.gd
+++ objects/items/item.gd
@@ -4,7 +4,7 @@ class_name Item extends Sprite2D
 @export var iArea : InteractionArea
 
 
-func _init(ticket: int, size: int, weight: int) -> void:
+func _init(ticket: int, size: int = 1, weight: float = 1) -> void:
 	itemData.ticket = ticket
 	itemData.size = size
 	itemData.weight = weight
