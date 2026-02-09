9583a9f2d9e60ab3177771919e9c62f0d8804821
Devanush Joseph <sek402@usask.ca>
2026-02-09 11:56:10 -0600

    Added signal for when add item fails

--- core/autoload/inventory/inventory.gd
+++ core/autoload/inventory/inventory.gd
@@ -1,9 +1,14 @@
 class_name Inventory extends Node
 
 signal held_item_changed(prev: Item, new: Item)
+signal add_held_item_failed
 
 var held_item : Item:
 	set = add_held_item
+
+
+func _ready() -> void:
+	held_item = null
 
 
 func has_item() -> bool:
@@ -12,6 +17,7 @@ func has_item() -> bool:
 
 func add_held_item(item: Item) -> bool:
 	if has_item():
+		add_held_item_failed.emit()
 		return false
 	else:
 		held_item_changed.emit(held_item, item)
