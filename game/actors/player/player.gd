2d75e7bb66651108aa336ac6ce6f00312edc6a66
Jasmine Pawliw <jasmine.pawliw@usask.ca>
2026-02-08 19:34:03 -0600

    feat: added sprite sheets and animations

--- game/actors/player/player.gd
+++ game/actors/player/player.gd
@@ -10,27 +10,13 @@ class_name Player extends CharacterBody2D
 
 #wasd is the movement for move_(direction). 
 func _physics_process(delta: float) -> void:
-	var move_vec = Vector2()
-	if Input.is_action_pressed("sprint"):
-		if Input.is_action_pressed("move_left"):
-			move_vec.x -= 1 * sprint_speed
-		if Input.is_action_pressed("move_right"):
-			move_vec.x += 1 * sprint_speed
-		if Input.is_action_pressed("move_up"):
-			move_vec.y -= 1 * sprint_speed
-		if Input.is_action_pressed("move_down"):
-			move_vec.y += 1 * sprint_speed
+	var input_direction = Input.get_vector('move_left', 'move_right', 'move_up', 'move_down')
+	
+	if (Input.is_action_pressed("sprint")):
+		move_and_collide(input_direction * delta * (move_speed * sprint_speed))
 	else:
-		if Input.is_action_pressed("move_left"):
-			move_vec.x -= 1
-		if Input.is_action_pressed("move_right"):
-			move_vec.x += 1
-		if Input.is_action_pressed("move_up"):
-			move_vec.y -= 1
-		if Input.is_action_pressed("move_down"):
-			move_vec.y += 1
-		
-	move_and_collide(move_vec * delta * move_speed)
+		move_and_collide(input_direction * delta * move_speed)
+
 	var screen_size = get_viewport_rect().size
 	position.x = clamp(position.x, 0, screen_size.x)
 	position.y = clamp(position.y, 0, screen_size.y)
