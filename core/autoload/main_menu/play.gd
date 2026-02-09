c3f60de54d3741410483af7080e19a2722b056ae
Mark Luu (ogt386) <ogt386@usask.ca>
2026-02-09 02:22:34 -0600

    added main menu

---
+++ core/autoload/main_menu/play.gd
@@ -0,0 +1,12 @@
+extends Button
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
+	
+func _on_button_up() -> void:
+	get_tree().change_scene_to_file("res://game.tscn") # Replace with function body.
