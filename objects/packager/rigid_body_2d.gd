53cb3f806fd1918450a73ab7e1383b2106f55567
Mark Luu (ogt386) <ogt386@usask.ca>
2026-02-10 01:13:04 -0600

    refactored shipper and prep for masks and manager

--- objects/packager/rigid_body_2d.gd
+++ objects/packager/rigid_body_2d.gd
@@ -1,6 +1,7 @@
-extends Area2D
+extends RigidBody2D
 
 @export var scanner = null
+@export var inside := false
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	$InteractionArea.interact = Callable(self, "_on_interact")
@@ -8,10 +9,10 @@ func _ready() -> void:
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	pass
+
 # on interacting with the box, energy decreases: 
 func _on_interact():
-	# DO NOT TAKE THE BOX MASK OFF LEVEL 3
-	scanner = get_overlapping_bodies()
+	scanner = $InteractionArea.get_overlapping_bodies()		
 	# rebind controls in editor -> editor settings -> shortcuts. remapped to E here
 	print(scanner)
 	for box in scanner:
