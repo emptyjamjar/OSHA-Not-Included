d1e829baa80eb16fd6d114af96b269bad7edfa95
Shane <skf899@usask.ca>
2026-02-11 13:50:54 -0600

    One of the many attempts to resolve issue with item not spawning correctly by calling _init() manually after instantiate.

--- levels/testing/conveyor/level_testing_conveyor_01.gd
+++ levels/testing/conveyor/level_testing_conveyor_01.gd
@@ -2,11 +2,15 @@ extends Node
 
 func _ready():
 	var conveyor := $Conveyor
-	var tp1 = preload("res://objects/items/toilet_paper/toilet_paper.tscn").instantiate() as ToiletPaper
-	var tp2 = preload("res://objects/items/toilet_paper/toilet_paper.tscn").instantiate() as ToiletPaper
-	var tp3 = preload("res://objects/items/toilet_paper/toilet_paper.tscn").instantiate() as ToiletPaper
-	var tp4 = preload("res://objects/items/toilet_paper/toilet_paper.tscn").instantiate() as ToiletPaper
-	
+	var tp1 := preload("res://objects/items/toilet_paper/toilet_paper.tscn").instantiate() as Item
+	tp1._init(1)
+	var tp2 := preload("res://objects/items/toilet_paper/toilet_paper.tscn").instantiate() as Item
+	tp2._init(2)
+	var tp3 := preload("res://objects/items/toilet_paper/toilet_paper.tscn").instantiate() as Item
+	tp3._init(3)
+	var tp4 := preload("res://objects/items/toilet_paper/toilet_paper.tscn").instantiate() as Item
+	tp4._init(4)
+
 	conveyor.input(tp1)
 	conveyor.input(tp2)
 	conveyor.input(tp3)
