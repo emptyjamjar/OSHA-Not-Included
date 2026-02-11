2bddc05ce19212d3bc0ad5082e0dc8a01e052204
Jasmine Pawliw <jasmine.pawliw@usask.ca>
2026-02-10 19:37:59 -0600

    fix: made box smaller so I could see animations

--- objects/shipper/shipper.gd
+++ objects/shipper/shipper.gd
@@ -3,6 +3,8 @@ extends Area2D
 @export var boxes = null
 @onready var animated_sprite = $AnimatedSprite2D
 
+var shipped:bool = false
+
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	$InteractionArea.interact = Callable(self, "_on_interact")
@@ -13,9 +15,10 @@ func _ready() -> void:
 func _on_body_entered(body:Node2D) -> void:
 	if (body.is_in_group("player")):
 		animated_sprite.play("open")
+		shipped = false
 	
 func _on_body_exited(body:Node2D) -> void:
-	if (body.is_in_group("player")):
+	if (body.is_in_group("player") and !shipped):
 		animated_sprite.play("close")
 
 			
@@ -25,3 +28,4 @@ func _on_interact():
 		if box.is_in_group("Shippable"):
 			box.free()
 			animated_sprite.play("drive_away")
+			shipped = true
