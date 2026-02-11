dcd6be4ddd3927db6357b4a990b4a5ffbbd4411b
Shane F <skf899@usask.ca>
2026-02-10 23:23:59 -0600

    Fixed wrong attached script. Added output_speed variable.

--- objects/conveyor/conveyor.gd
+++ objects/conveyor/conveyor.gd
@@ -1,1 +1,10 @@
 extends Node
+# The conveyor belt outputs items
+
+# Affects how quickly items are moved/Animation speed
+# Default: 5
+@export var conveyor_speed:int = 5
+
+# Affects how quickly items are output onto the conveyor belt
+# Default: 1
+@export var output_speed:int = 1
--- objects/conveyor/conveyor_controller.gd
+++ objects/conveyor/conveyor_controller.gd
@@ -2,5 +2,3 @@ extends Node
 
 ## This conveyor controller script handles the interactions and controls of
 ## nodes.
-
-@export var speed:int = 5
