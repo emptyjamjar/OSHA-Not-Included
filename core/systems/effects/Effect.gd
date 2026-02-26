48104e40e569f57ead34a1489b86dd24a1e702ef
Shane F <skf899@usask.ca>
2026-02-26 05:38:25 -0600

    fixed effect comments because it was bugging me.

--- core/systems/effects/Effect.gd
+++ core/systems/effects/Effect.gd
@@ -8,19 +8,19 @@ signal ended() # this effect has ended
 # Attributes #
 enum Type
 {
-	NONE, # placeholder type or can mean no effect
-	STRUCTURAL, # Affects buildings, walls, doors, or physical structures (like the level itself)
-	OBJECT, # Affects items, props, or movable objects
-	ATMOSPHERE, # changes weather, fog, lighting, or ambient mood
-	BIOLOGICAL, # Affects living things (health, stamina, sanity, etc.)
-	AUDIO, # Plays sounds, music, or voice lines
-	VISUAL, # Changes colors, overlays, screen effects, or particle systems
-	SIGNAGE, # Shows text, signs, or UI messages
-	TEMPORAL, # Alters time (slow-mo, fast-forward, time stop), or time based stuff
-	BUFF, # Positive effect (e.g., +speed, +health)
-	DEBUFF, # Negative effect (e.g., -vision, -stamina)
-	ENVIRONMENTAL, # Affects the world (gravity, wind, temperature, etc.)
-	HAZARD # Dangerous effect (fire, poison, radiation, etc.)
+	NONE,			# placeholder type or can mean no effect
+	STRUCTURAL,		# Affects buildings, walls, doors, or physical structures (like the level itself)
+	OBJECT,			# Affects items, props, or movable objects
+	ATMOSPHERE,		# changes weather, fog, lighting, or ambient mood
+	BIOLOGICAL,		# Affects living things (health, stamina, sanity, etc.)
+	AUDIO,			# Plays sounds, music, or voice lines
+	VISUAL,			# Changes colors, overlays, screen effects, or particle systems
+	SIGNAGE,		# Shows text, signs, or UI messages
+	TEMPORAL,		# Alters time (slow-mo, fast-forward, time stop), or time based stuff
+	BUFF,			# Positive effect (e.g., +speed, +health)
+	DEBUFF,			# Negative effect (e.g., -vision, -stamina)
+	ENVIRONMENTAL,	# Affects the world (gravity, wind, temperature, etc.)
+	HAZARD			# Dangerous effect (fire, poison, radiation, etc.)
 }
 
 var _type: Type = Type.NONE
