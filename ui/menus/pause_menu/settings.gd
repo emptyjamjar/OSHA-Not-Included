ff359b835b74407966c3eb94fdf8f60d6571953d
Devanush Joseph <sek402@usask.ca>
2026-02-28 16:05:01 -0600

    Adjusted proccess mode and moved settings menu spawn location

--- ui/menus/pause_menu/settings.gd
+++ ui/menus/pause_menu/settings.gd
@@ -1,20 +1,17 @@
-## Settings button for pause menu.
+## Settings button for menus.
 extends TextureButton
 
 signal settings_closed
 
 
-func _ready():
-	process_mode = Node.PROCESS_MODE_WHEN_PAUSED
-
-
 func _on_pressed() -> void:
 	var scene = preload("res://ui/menus/settings_menu/settings_menu.tscn")
 	var instance = scene.instantiate() as SettingsMenu
+	instance.global_position.y += 10
 	instance.exit_pressed.connect(_on_settings_exited.bind(instance))
 	add_child(instance)
 
 
-func _on_settings_exited(menu: SettingsMenu) -> void:
-	await menu.tree_exited
+func _on_settings_exited(settings: SettingsMenu) -> void:
+	await settings.tree_exited
 	settings_closed.emit()
