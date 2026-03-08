33a5e48ee718d5dbd0fd77ba68d4fdf9371a94d4
Jasmine Pawliw <jasmine.pawliw@usask.ca>
2026-03-08 17:03:52 -0600

    fix: made audio sliders convert db to linear

--- ui/menus/settings_menu/audio_settings/audio_slider.gd
+++ ui/menus/settings_menu/audio_settings/audio_slider.gd
@@ -10,9 +10,13 @@ class_name AudioSlider extends HBoxContainer
 @export var sliderLabel : Label
 
 func _ready() -> void:
+	volumeSlider.value_changed.disconnect(_on_slider_value_changed)
+	
 	muteBtn.button_pressed = not AudioServer.is_bus_mute(audioBusIndex)
-	volumeSlider.value = AudioServer.get_bus_volume_db(audioBusIndex)
+	volumeSlider.value = db_to_linear(AudioServer.get_bus_volume_db(audioBusIndex))
 	sliderLabel.text = str(roundi(volumeSlider.ratio * 100))
+	
+	volumeSlider.value_changed.connect(_on_slider_value_changed)
 
 
 func _on_check_box_toggled(toggled_on: bool) -> void:
@@ -20,5 +24,5 @@ func _on_check_box_toggled(toggled_on: bool) -> void:
 
 
 func _on_slider_value_changed(value: float) -> void:
-	AudioServer.set_bus_volume_db(audioBusIndex, value)
+	AudioServer.set_bus_volume_db(audioBusIndex, linear_to_db(value))
 	sliderLabel.text = str(roundi(volumeSlider.ratio * 100))
