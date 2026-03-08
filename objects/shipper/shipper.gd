00e689e4f6e3d08cc5d092b9b4fe3bcc036a237b
Jasmine Pawliw <jasmine.pawliw@usask.ca>
2026-03-08 14:18:40 -0600

    fix: terminal location

--- objects/shipper/shipper.gd
+++ objects/shipper/shipper.gd
@@ -53,15 +53,6 @@ func _on_interact():
 			animated_sprite.play("close")
 			shipped = true
 			player_collision.disabled = true
-			#timer.start()
-
-	# #animate truck only when ticket is complete
-	#if ticket_manager.active_ticket.status == Ticket.TicketStatus.REACHED_GOAL:
-		#get_money.emit()
-		#animated_sprite.play("drive_away")
-		#shipped = true
-		#player_collision.disabled = true
-		#timer.start()
 
 
 func _on_arrive_timer_timeout() -> void:
