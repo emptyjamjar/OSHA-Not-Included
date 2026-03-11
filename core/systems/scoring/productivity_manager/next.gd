3a789cc939b2098b3ad6a15cd44ce242964729d0
Jasmine Pawliw <jasmine.pawliw@usask.ca>
2026-03-11 12:08:04 -0600

    fix: added audio to next day button and hid hud on score

--- core/systems/scoring/productivity_manager/next.gd
+++ core/systems/scoring/productivity_manager/next.gd
@@ -13,4 +13,5 @@ func _process(delta: float) -> void:
 
 func _on_pressed() -> void:
 	get_tree().paused = false
+	Audio.play_click()
 	get_tree().change_scene_to_file("res://levels/warehouse_01/layout/game.tscn")
