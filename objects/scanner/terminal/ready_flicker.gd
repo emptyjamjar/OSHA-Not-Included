59cb3b19812a0a07395c164873afa1ac4c7bd7b4
Luu <ogt386@usask.ca>
2026-03-19 14:22:20 -0600

    actually saved flicker gd

---
+++ objects/scanner/terminal/ready_flicker.gd
@@ -0,0 +1,24 @@
+# Light controller for the terminal.
+
+extends PointLight2D
+@onready var timer = $LightTimer
+@onready var tickets = TicketManager
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	tickets.tickets_done.connect(_on_tickets_done)
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
+
+
+func _on_tickets_done() -> void:
+	visible = true
+	timer.start()
+
+
+func _on_timer_timeout() -> void:
+	visible = !visible
+	timer.start()
