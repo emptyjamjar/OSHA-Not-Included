f09d41a36cb34f25b3d7c9dfe6233028dd6736d6
Shane F <skf899@usask.ca>
2026-03-27 13:53:29 -0600

    Revamped some of the functionality of Effect to be more flexible. Removed a lot of bloat to Effect that I dont think are needed or people probably wont use. Added Cooldown to allow Effect to have gaps between updates. Added Flags for timing, reapts, and cooldown. Repats and duration now play nicely with each other, allowing duration to work with repeats. Added signals by default to enter and exit API methods.

--- core/systems/effects/Effect.gd
+++ core/systems/effects/Effect.gd
@@ -22,15 +22,39 @@ enum Type
 	ENVIRONMENTAL,	# Affects the world (gravity, wind, temperature, etc.)
 	HAZARD			# Dangerous effect (fire, poison, radiation, etc.)
 }
-
+# basic info
 var _type: Type = Type.NONE
 var _effect_name: String = ""
+
+# flags
+## Only one of this effect may be active
 var _is_unique: bool = false
-var _is_persistent: bool = false # is this effect always on? (until manually turned off)
-var _duration: float = 0.0 # how long should this effect be active for (in seconds)
-var _repeat_count: int = 0 # how many times to repeat this effect
+## is this effect always on? (until manually turned off)
+var _is_persistent: bool = false
+
+# Timing
+## Enable effect timing/duration
+var _enable_timing:bool = false
+## how long should this effect be active for (in seconds) or until next repeat
+var _duration: float = 0.0 
+## Time elapsed so far
 var _elapsed_time: float = 0.0 # time active
-var _repeat_index: int = 0 # tracks current cycle (ex: 5th cycle)
+
+# Repeating
+## Enable effect repeating
+var _enable_repeat:bool = false
+## how many times to repeat this effect
+var _repeat_max: int = 0
+## Repeats so far
+var _repeat_count: int = 0
+
+# cooldown
+## Enable cooldown after effect ends before it can be applied again
+var _enable_cooldown: bool = false
+## Cooldown duration in seconds
+var _cooldown_duration: float = 0.0
+## Time elapsed in cooldown
+var _cooldown_elapsed: float = 0.0
 
 # Basic constructor for Effect
 func _init(type: Type = Type.NONE, effect_name: String = "No_Name") -> void:
@@ -42,11 +66,13 @@ func _init(type: Type = Type.NONE, effect_name: String = "No_Name") -> void:
 ## Called when the effect starts (e.g., play sound, start particles)
 ## @param delta: float - Time since last frame (for timing)
 func enter(delta: float) -> void:
+	started.emit()
 	pass
 
 ## Called when the effect ends (e.g., stop sound, remove particles)
 ## @param delta: float - Time since last frame
 func exit(delta: float) -> void:
+	ended.emit()
 	pass
 
 ## Called every frame while the effect is active (for visual/audio updates)
@@ -59,11 +85,14 @@ func update(delta: float) -> void:
 func physics_update(delta: float) -> void:
 	pass
 
+
 # Getters/Setters #
+
+
 ## Sets the type of effect (e.g BUFF, VISUAL, HAZARD)
 ## @param new_type: Type - The new effect type to assign
 func set_type(new_type: Type) -> void:
-	_type = new_type
+	self._type = new_type
 
 ## Gets the current effect type
 ## @return: Type - The current effect type
@@ -73,7 +102,7 @@ func get_type() -> Type:
 ## Sets the name of the effect (for debugging or logging)
 ## @param new_name: String - The new name to assign
 func set_effect_name(new_name: String) -> void:
-	_effect_name = new_name
+	self._effect_name = new_name
 
 ## Gets the name of the effect
 ## @return: String - The current name of the effect
@@ -81,9 +110,9 @@ func get_effect_name() -> String:
 	return _effect_name
 
 ## Sets whether only one instance of this effect can be active at once
-## @param is_unique: bool - True if only one instance allowed
-func set_unique(is_unique: bool) -> void:
-	_is_unique = is_unique
+## @param enable: bool - True if only one instance allowed
+func set_unique(enable: bool) -> void:
+	self._is_unique = enable
 
 ## Checks if only one instance of this effect can be active
 ## @return: bool - True if unique, false otherwise
@@ -91,152 +120,205 @@ func is_unique() -> bool:
 	return _is_unique
 
 ## Sets whether the effect stays active until manually stopped
-## @param is_persistent: bool - True if effect never expires
-func set_persistent(is_persistent: bool) -> void:
-	_is_persistent = is_persistent
+## @param enable: bool - True if effect never expires
+func set_persistent(enable: bool) -> void:
+	self._is_persistent = enable
 
 ## Checks if the effect is persistent (never expires)
 ## @return: bool - True if persistent, false otherwise
 func is_persistent() -> bool:
-	return _is_persistent
+	return self._is_persistent
+
+## Enables or disables timing for the effect (if disabled, duration is ignored and effect must be manually stopped)
+## @param enable: bool - True to enable timing, false to disable
+func set_enable_timing(enable: bool) -> void:
+	self._enable_timing = enable
+
+## Checks if timing is enabled for the effect
+## @return: bool - True if timing enabled, false otherwise
+func is_timing_enabled() -> bool:
+	return self._enable_timing
 
 ## Sets the duration of the effect (in seconds)
 ## @param new_duration: float - Duration in seconds (0 = instant)
 func set_duration(new_duration: float) -> void:
-	_duration = new_duration
+	self._duration = new_duration
 
 ## Gets the duration of the effect
 ## @return: float - Duration in seconds
 func get_duration() -> float:
-	return _duration
+	return self._duration
+
+## Enables or disables repeating for the effect (if disabled, repeat count is ignored and effect must be manually stopped)
+## @param enable: bool - True to enable repeating, false to disable
+func set_enable_repeat(enable: bool) -> void:
+	self._enable_repeat = enable
+
+## Checks if repeating is enabled for the effect
+## @return: bool - True if repeating enabled, false otherwise
+func is_repeat_enabled() -> bool:
+	return self._enable_repeat
 
 ## Sets how many times the effect should repeat
 ## @param new_count: int - Number of times to repeat (0 = no repeat)
-func set_repeat_count(new_count: int) -> void:
-	_repeat_count = new_count
+## @return: bool - True if set successfully, false if invalid count
+func set_repeat_max(new_count: int) -> bool:
+	if new_count < 0:
+		return false
+	self._repeat_max = new_count
+	return true
 
 ## Gets the number of times the effect should repeat
 ## @return: int - Number of repeats
-func get_repeat_count() -> int:
-	return _repeat_count
+func get_repeat_max() -> int:
+	return self._repeat_max
 
 ## Sets the elapsed time since the effect started
 ## @param new_time: float - Time in seconds since effect began
-func set_elapsed_time(new_time: float) -> void:
-	_elapsed_time = new_time
+## @return: bool - True if set successfully, false if invalid time
+func set_elapsed_time(new_time: float) -> bool:
+	if new_time < 0:
+		return false
+	self._elapsed_time = new_time
+	return true
 
 ## Gets the elapsed time since the effect started
 ## @return: float - Time in seconds
 func get_elapsed_time() -> float:
-	return _elapsed_time
+	return self._elapsed_time
 
 ## Sets the current repeat cycle index
-## @param new_index: int - Current repeat count (0 = first)
-func set_repeat_index(new_index: int) -> void:
-	_repeat_index = new_index
+## @param new_count: int - Current repeat count (0 = first)
+## @return: bool - True if set successfully, false if invalid count
+func set_repeat_count(new_count: int) -> bool:
+	if new_count < 0:
+		return false
+	self._repeat_count = new_count
+	return true
 
 ## Gets the current repeat cycle index
 ## @return: int - Current repeat count
-func get_repeat_index() -> int:
-	return _repeat_index
+func get_repeat_count() -> int:
+	return self._repeat_count
 
-# Helper and utility methods #
+## Enables or disables cooldown for the effect (if enabled, effect cannot be applied again until cooldown expires)
+## @param enable: bool - True to enable cooldown, false to disable
+func set_enable_cooldown(enable: bool) -> void:
+	self._enable_cooldown = enable
 
-## Resets the effect to its initial state (for reuse or restarting)
-## @param reset_elapsed: bool - Whether to reset elapsed time and repeat index
-func reset(reset_elapsed: bool = true) -> void:
-	_elapsed_time = 0.0 if reset_elapsed else _elapsed_time
-	_repeat_index = 0 if reset_elapsed else _repeat_index
-	_repeat_count = 0  # Reset repeat counter (can be incremented on next cycle)
+## Checks if cooldown is enabled for the effect
+## @return: bool - True if cooldown enabled, false otherwise
+func is_cooldown_enabled() -> bool:
+	return self._enable_cooldown
 
-## Increments the repeat index and count (called after each cycle)
+## Sets the cooldown duration after the effect ends (in seconds)
+## @param new_duration: float - Cooldown duration in seconds
+## @return: bool - True if set successfully, false if invalid duration
+func set_cooldown_duration(new_duration: float) -> bool:
+	if new_duration < 0:
+		return false
+	self._cooldown_duration = new_duration
+	return true
+
+## Gets the cooldown duration after the effect ends
+## @return: float - Cooldown duration in seconds
+func get_cooldown_duration() -> float:
+	return self._cooldown_duration
+
+
+# Reset methods #
+
+## Resets the effect to its initial state
+func reset() -> void:
+	# reset timing
+	self._elapsed_time = 0.0
+	# reset repeating
+	self._repeat_count = 0
+	# reset cooldown
+	self._cooldown_elapsed = 0.0
+
+## Resets only the timing (elapsed time) of the effect
+func reset_timing() -> void:
+	self._elapsed_time = 0.0
+
+## Resets only the repeating (repeat count) of the effect
+func reset_repeating() -> void:
+	self._repeat_count = 0
+
+## Resets only the cooldown (cooldown elapsed time) of the effect
+func reset_cooldown() -> void:
+	self._cooldown_elapsed = 0.0
+
+# Incremental methods #
+
+## Increments the repeat count (called after each cycle)
 func increment_repeat() -> void:
-	_repeat_index += 1
-	_repeat_count += 1
+	self._repeat_count += 1
 
-## Restarts the effect (resets time, triggers enter again)
-## @param delta: float - Time since last frame (for timing)
-func restart(delta: float) -> void:
-	reset(true)
-	enter(delta)
+## Increments the elapsed time by the given delta (called each frame)
+## @param delta: float - Time in seconds to add to elapsed time
+func increment_elapsed_time(delta: float) -> bool:
+	if delta < 0:
+		return false
+	self._elapsed_time += delta
+	return true
+
+## Increments the cooldown elapsed time by the given delta (called each frame)
+## @param delta: float - Time in seconds to add to cooldown elapsed time
+func increment_cooldown_elapsed(delta: float) -> bool:
+	if delta < 0:
+		return false
+	self._cooldown_elapsed += delta
+	return true
+
 
 # Query methods #
 
-## Checks if the effect should still be active (based on duration and persistence)
+## Checks if the effect is still active (duration, repeating)
 ## @return: bool- True if active, false if expired
 func is_active() -> bool:
-	if _is_persistent:
+	# if persistent, always active
+	if self._is_persistent:
 		return true
-	if _duration <= 0:
-		return false  # Instant effect (consider it expired after start() )
-	return _elapsed_time < _duration
+	# if timing enabled and duration exceeded, not active
+	if self._enable_timing and self._elapsed_time >= self._duration:
+		return false
+	# if repeating enabled and repeat count reached, not active
+	if self._enable_repeat and self._repeat_max > 0 and self._repeat_count >= self._repeat_max:
+		return false
+	return true
 
 ## Checks if the effect has finished (expired and not persistent)
 ## @return: bool - True if finished, false if still active
 func is_finished() -> bool:
 	return not is_active()
 
-## Checks if the effect is in its first cycle (repeat_index == 0)
-## @return: bool - True if first cycle, false otherwise
-func is_first_cycle() -> bool:
-	return _repeat_index == 0
+## Checks if the effect is currently on cooldown (after finishing and before it can be applied again)
+## @return: bool - True if on cooldown, false otherwise
+func is_on_cooldown() -> bool:
+	if not self._enable_cooldown:
+		return false
+	return self._cooldown_elapsed < self._cooldown_duration
 
 ## Gets the percentage of duration completed (0.0 to 1.0)
 ## @return: float - Progress as percentage (0.0 = start, 1.0 = end)
 func get_progress() -> float:
-	if _duration <= 0:
+	if self._duration <= 0:
 		return 1.0
-	return min(_elapsed_time / _duration, 1.0)
+	return min(self._elapsed_time / self._duration, 1.0)
 
 ## Gets the remaining time until effect ends (0 if persistent or expired)
 ## @return: float: Seconds left, or 0 if persistent/expired
 func get_remaining_time() -> float:
-	if _is_persistent:
+	if self._is_persistent:
 		return 0.0
-	if _elapsed_time >= _duration:
+	if self._elapsed_time >= self._duration:
 		return 0.0
-	return _duration - _elapsed_time
+	return self._duration - self._elapsed_time
 
-## Gets the current repeat cycle as a percentage (0.0 to 1.0)
-## @return: float - Progress through current cycle (0.0 = start, 1.0 = end)
-func get_cycle_progress() -> float:
-	if _duration <= 0:
-		return 1.0
-	return min(_elapsed_time / _duration, 1.0)
+# helper methods #
 
-## Gets the total number of cycles completed (including current)
-## @return: int - Number of cycles completed so far
-func get_completed_cycles() -> int:
-	return _repeat_index
-
-## Checks if the effect is a "buff" (positive effect)
-## @return: bool - True if type is BUFF, false otherwise
-func is_buff() -> bool:
-	return _type == Type.BUFF
-
-## Checks if the effect is a "debuff" (negative effect)
-## @return: bool - True if type is DEBUFF, false otherwise
-func is_debuff() -> bool:
-	return _type == Type.DEBUFF
-
-## Gets the effect’s type as a string (e.g., "BUFF", "HAZARD")
+## Gets the effect’s type as a string ("BUFF", "HAZARD")
 ## @return: String - Type name without "Type." (honestly great to have)
-func get_type_name() -> String:
-	return str(_type).replace("Type.", "")
-
-## Checks if the effect is "instant' (duration == 0)
-## @return: bool - True if instant, false otherwise
-func is_instant() -> bool:
-	return _duration == 0.0
-
-## Gets a string status ("Active (50% done)", "Finished", "Persistent")
-## @return: String - Status message for UI or logs
-func get_status() -> String:
-	if _is_persistent:
-		return "Persistent"
-	if _elapsed_time >= _duration:
-		return "Finished"
-	if _duration > 0:
-		var percent = int(get_progress() * 100)
-		return "Active (%d%% done)" % percent
-	return "Instant"
+func get_type_as_string() -> String:
+	return str(self._type).replace("Type.", "")
