bd615ef85e11716e20f5489b91c15cd2f590cfc0
Jasmine Pawliw <jasmine.pawliw@usask.ca>
2026-03-29 14:04:16 -0600

    feat: added item overlays

--- ui/hud/inventory/inventory_hud.gd
+++ ui/hud/inventory/inventory_hud.gd
@@ -3,16 +3,21 @@ extends TextureRect
 
 @export var slot1 : TextureRect
 @export var slot1Btn : TextureButton
+@export var boxedItem1: TextureRect
 @export var slot2 : TextureRect
 @export var slot2Btn : TextureButton
+@export var boxedItem2: TextureRect
 @export var AnimPlayer: AnimationPlayer
 
 var slots : Array[TextureRect] = []
+var boxed_item_slots : Array[TextureRect] = []
 
 
 func _ready() -> void:
 	slots.push_back(slot1)
 	slots.push_back(slot2)
+	boxed_item_slots.push_back(boxedItem1)
+	boxed_item_slots.push_back(boxedItem2)
 	PlayerInventory.storage_updated.connect(_on_inventory_updated)
 	InteractionManager.invalid_interact.connect(_hud_shake)
 
@@ -31,7 +36,7 @@ func _hud_shake():
 
 
 func _on_inventory_updated():
-	# Update currently selected slot
+	# Update currently selected slot 
 	match PlayerInventory.selectedIndex:
 		0:
 			slot1Btn.button_pressed = true
@@ -41,8 +46,11 @@ func _on_inventory_updated():
 	# Update inventory slots
 	for i in PlayerInventory.max_capacity:
 		if PlayerInventory.contents[i] != null:
-			slots[i].texture = PlayerInventory.contents[i].uiTexture
+			slots[i].texture = PlayerInventory.contents[i].texture
 			slots[i].tooltip_text = PlayerInventory.contents[i].name + "\n" + PlayerInventory.contents[i].description
+			if PlayerInventory.contents[i].type == ItemData.Type.PACKAGE:
+				boxed_item_slots[i].texture = PlayerInventory.contents[i].uiTexture
 		else:
 			slots[i].texture = null
 			slots[i].tooltip_text = ""
+			boxed_item_slots[i].texture = null
