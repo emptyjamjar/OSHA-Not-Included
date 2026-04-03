0cafa08abf65488f55ef3de08717cc31d4d4f95c
Daniel <ofh293@usask.ca>
2026-04-02 23:38:47 -0600

    There is now a penalty for going below the clock time

--- ui/hud/hud.gd
+++ ui/hud/hud.gd
@@ -10,10 +10,9 @@ extends CanvasLayer
 @onready var needscomp: NeedsComponent = $"../../Player/NeedsComponent"
 @onready var sanitycomp: SanityComponent = $"../../Player/SanityComponent"
 
-@export var clock: Label
-@export var clock_no_ticket: Label
-var clock_time:float = 0
-var clock_start:bool = false #If the timer should start.
+@export var clock: LevelClock
+
+@export var productivity_manager: ProductivityManager
 
 @export var quota: Label
 var quota_size: int
@@ -32,7 +31,11 @@ func _ready() -> void:
 	needsbar.max_value = needscomp.MAX_NEEDS
 	needsbar.value = needscomp.needs
 	
-	Ticket_Manager.tickets_generated.connect(start_clock)
+	if productivity_manager == null:
+		printerr("Connect Score Node (Under UI) to HUD's productivity_manager export var (Under Camera2D)")
+	##IMPORTANT: READ THE ERROR ABOVE THIS MESSAGE IF YOU GET AN ERROR POINTING TO HERE.
+	print(productivity_manager)
+	clock.deduct_productivity.connect(productivity_manager.add_productivity)
 	
 	Ticket_Manager.ticket_timed_out.connect(_missed_quota)
 	Ticket_Manager.ticket_submitted.connect(_submitted_quota)
@@ -46,50 +49,11 @@ func _process(delta: float) -> void:
 	else:
 		energycomp.draining = false
 	
-	update_clock(delta)
 	_update_quota()
 
 
-func get_clock_time()->float:
-	var tickets: Array[Ticket] = Ticket_Manager.all_tickets
-	var sum_time: float = 0
-	
-	for ticket in tickets:
-		sum_time += ticket.max_time
-	
-	return sum_time
-
-
-func start_clock():
-	clock_start = true
-	clock_time = get_clock_time()
-
-
-func update_clock(delta: float):
-	if clock_start:
-		clock.visible = true
-		clock_no_ticket.visible = false
-		
-		clock_time -= delta
-		
-		var minutes: float = fmod(clock_time, 60.0)
-		var minutes_text: String = str(int(minutes))
-		
-		#Just makes sure that single digit seconds fit the clock
-		if minutes < 10:
-			minutes_text = "0" + str(int(minutes))
-		elif minutes < 0:
-			minutes_text = "00"
-		
-		clock.text = str(int(clock_time/60)) +  ":" + minutes_text
-	else:
-		clock.visible = false
-		clock_no_ticket.visible = true
-
-
 func _update_quota():
-	if clock_start:
-		quota.text = str(Ticket_Manager.all_tickets.size() + Ticket_Manager.visible_queue.size())
+	quota.text = str(Ticket_Manager.all_tickets.size() + Ticket_Manager.visible_queue.size())
 
 
 ##TODO: Add animation.
