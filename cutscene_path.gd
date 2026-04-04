773d3469d7f2f4969d2ba2d3b320873ce93adf90
LoganWeckert <kqj634@usask.ca>
2026-04-04 01:08:42 -0600

    Srite for fake manager changed, text boxes have been updated to be smaller, waits for player interaction before advancing dialogue

--- cutscene_path.gd
+++ cutscene_path.gd
@@ -3,22 +3,68 @@ extends Path2D
 @export var speed = 100 # Pixels per second
 var state := 0
 
+@onready var animated_sprite_2d: AnimatedSprite2D = $PathFollow2D/AnimatedSprite2D
+
+
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	pass # Replace with function body.
 
 func _process(delta):
 	if state == 0:
-		$PathFollow2D/Sprite2D.show()
+		animated_sprite_2d.play("Idle_down")
+		#animated_sprite_2d.show()
 	elif $PathFollow2D.progress <= 286 && state == 1:
+		#384, 351
+		animated_sprite_2d.flip_h = true
+		animated_sprite_2d.play("Move_side")
 		$PathFollow2D.progress += speed * delta   
-	elif $PathFollow2D.progress <= 473 && state == 2:
+	elif $PathFollow2D.progress <= 407 && state == 2:
+		#197, 351
+		animated_sprite_2d.play("Move_side")
+		$PathFollow2D.progress += speed * delta
+	elif $PathFollow2D.progress <= 473 && state == 3:
+		#197, 285
+		animated_sprite_2d.play("Move_up")
+		$PathFollow2D.progress += speed * delta
+	elif $PathFollow2D.progress <= 897 && state == 3:
+		#621, 285
+		animated_sprite_2d.flip_h = false
+		animated_sprite_2d.play("Move_side")
+		$PathFollow2D.progress += speed * delta
+	elif $PathFollow2D.progress <= 961 && state == 3:
+		#621, 221
+		animated_sprite_2d.play("Move_up")
 		$PathFollow2D.progress += speed * delta
 	elif $PathFollow2D.progress <= 1027 && state == 3:
+		animated_sprite_2d.flip_h = true
+		animated_sprite_2d.play("Move_side")
+		$PathFollow2D.progress += speed * delta
+	elif $PathFollow2D.progress <= 1337 && state == 4:
+		#245, 221
+		animated_sprite_2d.flip_h = true
+		animated_sprite_2d.play("Move_side")
 		$PathFollow2D.progress += speed * delta
 	elif $PathFollow2D.progress <= 1403 && state == 4:
+		#245, 333
+		animated_sprite_2d.play("Move_down")
 		$PathFollow2D.progress += speed * delta
-	elif $PathFollow2D.progress < 2039 && state == 5:
+	elif $PathFollow2D.progress <= 1543 && state == 5:
+		#385, 333
+		animated_sprite_2d.flip_h = false
+		animated_sprite_2d.play("Move_down")
 		$PathFollow2D.progress += speed * delta
+	elif $PathFollow2D.progress <= 1815 && state == 5:
+		#657, 333
+		animated_sprite_2d.flip_h = false
+		animated_sprite_2d.play("Move_side")
+		$PathFollow2D.progress += speed * delta
+	elif $PathFollow2D.progress < 2194 && state == 5:
+		animated_sprite_2d.play("Move_down")
+		$PathFollow2D.progress += speed * delta
+	elif state == 1:
+		animated_sprite_2d.play("Idle_down")
+		Dialogic.paused = false
+		Dialogic.Text.show_textbox()
 	else:
-		$PathFollow2D/Sprite2D.hide()
+		animated_sprite_2d.play("Idle_down")
--- objects/scanner/terminal/ticket_terminal.gd
+++ objects/scanner/terminal/ticket_terminal.gd
@@ -3,12 +3,15 @@ class_name TicketTerminal
 
 signal ticketsEmpty
 signal activated
+signal ticket_dialogue
 @export var ticket: Ticket
 
 @onready var interaction_area: InteractionArea = $InteractionArea
 @onready var player_collision = $StaticBody2D/PlayerCollision
 @onready var ticket_queue_ui: CanvasLayer = $TicketQueueUI
+@onready var game = get_tree().get_first_node_in_group("game")
 @onready var player = get_tree().get_first_node_in_group("player")
+var first_interact := false
 
 var active := false 
 var ticket_counter := 0 
@@ -24,7 +27,10 @@ func _ready() -> void:
 
 	
 func _on_interact(): 
-	if not active: 
+	if not active:
+		if first_interact == false && game.game_state == 0:
+			emit_signal("ticket_dialogue")
+			first_interact = true
 		active = true
 		print("Terminal activated")
 		print("Templates at interact:", Ticket_Manager.ticket_templates.size())
