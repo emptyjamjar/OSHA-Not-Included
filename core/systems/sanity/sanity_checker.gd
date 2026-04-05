e243c10ebfad1b803f9119e9457119462c363533
Devanush Joseph <sek402@usask.ca>
2026-04-05 00:49:00 -0600

    Eyeballs now spawn and follow player

--- core/systems/sanity/sanity_checker.gd
+++ core/systems/sanity/sanity_checker.gd
@@ -1,8 +1,10 @@
 ## Checks sanity level and shows relevant effects
+## Make sure to connect the SanityComponent and Player
 extends Node
 class_name SanityChecker
 
 @export var sanity : SanityComponent
+@export var player : Player
 
 ## To use add SanityEyeball nodes to the scene and to the array of eyeballs
 ## For each eyeball, add desired effect to milestones array
@@ -26,6 +28,9 @@ func _ready() -> void:
 	# Hide all eyeballs
 	for eyeball in eyeballs:
 		eyeball.visible = false
+		eyeball.player = player
+		eyeball.enabled = false
+		eyeball.hitbox.call_deferred("set_disabled", true)
 
 
 func _on_sanity_changed(val: int):
@@ -40,7 +45,11 @@ func _on_sanity_changed(val: int):
 func _spawn_eye_ball(is_on: bool = false):
 	if is_on and _activated_eyeballs < eyeballs.size():
 		eyeballs[_activated_eyeballs].visible = true
+		eyeballs[_activated_eyeballs].enabled = true
+		eyeballs[_activated_eyeballs].hitbox.call_deferred("set_disabled", false)
 		_activated_eyeballs += 1
 	elif not is_on and _activated_eyeballs >= eyeballs.size():
-		eyeballs[_activated_eyeballs].visible = false
 		_activated_eyeballs -= 1
+		eyeballs[_activated_eyeballs].visible = false
+		eyeballs[_activated_eyeballs].enabled = false
+		eyeballs[_activated_eyeballs].hitbox.call_deferred("set_disabled", true)
--- objects/sanity_eye_ball/sanity_eyeball.gd
+++ objects/sanity_eye_ball/sanity_eyeball.gd
@@ -3,12 +3,14 @@ class_name SanityEyeball
 
 @export var irisSprite : Sprite2D
 @export var iris : CharacterBody2D
-@export var player : Player
+@export var hitbox : CollisionPolygon2D
 
+var enabled : bool = false
+var player : Player
 var speed : int = 25
 
 func _process(_delta: float) -> void:
-	if player:
+	if player and enabled:
 		iris.velocity = iris.global_position.direction_to(player.global_position)
 		iris.velocity *= speed
 		iris.move_and_slide()
