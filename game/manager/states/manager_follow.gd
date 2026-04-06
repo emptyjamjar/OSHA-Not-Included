1791d575da5fb798ab9302a1abf36976dc6613c1
Phong Thanh Nguyen <88289224+NaronSSS@users.noreply.github.com>
2026-04-06 11:24:51 -0600

    Manager code redo and adjust paths for each level

--- game/manager/states/manager_follow.gd
+++ game/manager/states/manager_follow.gd
@@ -11,15 +11,14 @@ func Enter():
 func Physics_Update(delta: float) -> void:
 	# get the direction between player and manager
 	var direction = player.global_position - manager.global_position
-	
-	if direction.length() > 25: 
-		# move manager towards the player
-		manager.velocity = direction.normalized() * move_speed
-	else: 
-		# stands still
-		manager.velocity = Vector2()
-		 
-	if direction.length() > 50: 
-		transitioned.emit(self, "idle")
-	 
+	manager.velocity = direction.normalized() * move_speed
+	#if direction.length() > 25: 
+		## move manager towards the player
+		#manager.velocity = direction.normalized() * move_speed
+	#else: 
+		## stands still
+		#manager.velocity = Vector2()
+		 #
+	#if direction.length() > 50: 
+		#transitioned.emit(self, "idle")
 	 
--- game/manager/states/manager_patrol.gd
+++ game/manager/states/manager_patrol.gd
@@ -3,19 +3,41 @@ class_name ManagerPatrol
 
 @export var manager: CharacterBody2D
 var player: CharacterBody2D
+var animated_sprite : AnimatedSprite2D 
 @export var move_speed := 40.0
-
-# Compute the bouding box of all tiles you've painted 
-
-
+var move_direction : Vector2
 var index := 0
 var wait_timer := 0.0
 
+# for checking animation midway along the path 
+var checking := false 
+var check_timer := 0.0
+@export var check_chance := 0.3 # 30% chance to stop and check 
+@export var check_min_time := 1.0 # seconds
+@export var check_max_time := 2.5
+
 func Enter():
 	player = get_tree().get_first_node_in_group("player")
+	animated_sprite = manager.get_node("AnimatedSprite2D")
 	wait_timer = 0.0
+	checking = false 
+	check_timer = 0.0
 	
 func Physics_Update(delta: float) -> void:
+	# If player is visible, stop patrolling
+	if manager.vision_cone.is_entity_visible(player):
+		transitioned.emit(self, "follow")
+		return
+	# checking animation 
+	if checking: 
+		animated_sprite.play("check_board")
+		check_timer -= delta
+		if check_timer <= 0.0: 
+			# done checking : re-enable vision + resume patrol
+			checking = false
+			manager.vision_cone.enable()
+		return
+	# normal behaviour
 	if manager.waiting: 
 		return
 	var path = manager.current_path
@@ -41,4 +63,21 @@ func Physics_Update(delta: float) -> void:
 			## wait time logic here
 			#pass 
 			
+func update_animation(): 
+	if move_direction == Vector2.ZERO: 
+		return # no movement, keep current animation 
 	
+	if abs(move_direction.x) > abs(move_direction.y): 
+		# horizontal movement
+		if move_direction.x > 0: 
+			animated_sprite.flip_h = false
+			animated_sprite.play("move_right")
+		else: 
+			animated_sprite.flip_h = true
+			animated_sprite.play("move_right")
+	else: 
+		# vertical movement
+		if move_direction.y > 0: 
+			animated_sprite.play("move_down")
+		else: 
+			animated_sprite.play("move_up")
