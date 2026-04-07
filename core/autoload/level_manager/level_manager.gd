11b56f05d8f12a88af5a9dff54bf75cf9d9f5e38
LoganWeckert <kqj634@usask.ca>
2026-04-07 05:30:30 -0600

    Turned on ysort and added the last two cutscenes woops

--- core/autoload/level_manager/level_manager.gd
+++ core/autoload/level_manager/level_manager.gd
@@ -9,7 +9,9 @@ var levels : Array[NodePath] = [
 	"res://levels/warehouse_01/layout/game.tscn",
 	"res://levels/cutscenes/cutscene01.tscn",
 	"res://levels/warehouse_01/layout/game_2.tscn",
+	"res://levels/cutscenes/cutscene02.tscn",
 	"res://levels/warehouse_02/game.tscn",
+	"res://levels/cutscenes/cutscene03.tscn",
 	"res://levels/warehouse_02/game_2.tscn"]
 
 var quota = 0
